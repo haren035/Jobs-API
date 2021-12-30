5147e03f25c8bcb8a6b545d021e83e4169da18e3
Haren Sah <haren3035@gmail.com>
2021-12-30 15:06:48 +1100

    Delete job controller work

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -63,7 +63,21 @@ const updateJob = async (req, res) => {
 
 //DELETE JOB
 const deleteJob = async (req, res) => {
-    res.send('delete job')
+    const {
+        user: { userId },
+        params: { id: jobId },
+    } = req
+
+    const job = await Job.findByIdAndRemove({
+        _id:jobId,
+        createdBy:userId
+    })
+
+    if (!job) { 
+        throw new NotFoundError(`No job with id ${jobId}`)
+    }
+    res.status(StatusCodes.OK).send("Delettion Success!")
+
 }
 
 
