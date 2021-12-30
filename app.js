d8f1ea44092c50de36a350e3ef86469b5009bd01
Haren Sah <haren3035@gmail.com>
2021-12-30 16:04:37 +1100

    add extra security packages

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 require('dotenv').config();
 require('express-async-errors');
+
+//Extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimitter = require('express-rate-limit')
+
+
 const express = require('express');
 const app = express();
 
@@ -15,8 +23,15 @@ const jobsRouter = require('./routes/jobs')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1)
+rateLimitter({ windowMs:60* 1000, max: 100})
 app.use(express.json());
-// extra packages
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+app.get('/'), (req,res) => {
+  res.send('jobs api')
+}
 
 // routes
 app.use('/api/v1/auth', authRouter)
